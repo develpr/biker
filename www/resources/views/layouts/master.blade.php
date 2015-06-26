c7758261d9e6c947c3c78888a5d3dcc9a125d1e4
develpr <kevin@develpr.com>
2015-06-26 09:31:31 -0400

    change to https links for fonts/shiv

--- www/resources/views/layouts/master.blade.php
+++ www/resources/views/layouts/master.blade.php
@@ -3,10 +3,10 @@
 <head>
 	<meta charset="utf-8">
 	<title>@yield('title', "Hello!") | Alexa, Find Me A Bike</title>
-	<link href='http://fonts.googleapis.com/css?family=Euphoria+Script' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Euphoria+Script' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="css/master.css">
 	<!--[if IE]>
-	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 	<script>
